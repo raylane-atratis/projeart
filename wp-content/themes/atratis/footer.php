934998adad2b55295759ca62efc0082030ff6cfb
david-mendes-s <david.a.mendes.s@gmail.com>
2025-09-17 14:00:58 -0300

    [Ajustes] - Menu Ouvidoria

--- wp-content/themes/atratis/footer.php
+++ wp-content/themes/atratis/footer.php
@@ -1,27 +1,29 @@
-<?php 
-    $logo_svg = get_field('logo_svg', 'option');
-    $logo_png = get_field('logo_png', 'option');
-    $logo_png_branco = get_field('logo_png_branca', 'option');
+<?php
+$logo_svg = get_field('logo_svg', 'option');
+$logo_png = get_field('logo_png', 'option');
+$logo_png_branco = get_field('logo_png_branca', 'option');
 
-    $background_footer = get_field('background_footer', 'option');
-    $redes = get_field('redes_sociais', 'option');
+$background_footer = get_field('background_footer', 'option');
+$redes = get_field('redes_sociais', 'option');
 
-    $descricao_footer = get_field('descricao_footer', 'option');
-    
-    $endereco = get_field('endereco', 'option');
-    $link_endereco = get_field('link_endereco', 'option');
-    $contatos_atendimento = get_field('contatos_atendimento', 'option');
+$descricao_footer = get_field('descricao_footer', 'option');
 
-    $lista_contatos = get_field('lista_contatos', 'option');
+$endereco = get_field('endereco', 'option');
+$link_endereco = get_field('link_endereco', 'option');
+$contatos_atendimento = get_field('contatos_atendimento', 'option');
 
-    $link_de_compra_dos_produtos = get_field('link_de_compra_dos_produtos', 'option');
+$lista_contatos = get_field('lista_contatos', 'option');
 
-    $cnpj = get_field('cnpj', 'option');
+$link_de_compra_dos_produtos = get_field('link_de_compra_dos_produtos', 'option');
 
-    $titulo_newsletter = get_field('titulo_newsletter', 'option');
-    $descricao_newsletter = get_field('descricao_newsletter', 'option');
-    $texto_cta_newsletter = get_field('texto_cta_newsletter', 'option');
-    $link_cta_newsletter = get_field('link_cta_newsletter', 'option');
+$cnpj = get_field('cnpj', 'option');
+
+$titulo_newsletter = get_field('titulo_newsletter', 'option');
+$descricao_newsletter = get_field('descricao_newsletter', 'option');
+$texto_cta_newsletter = get_field('texto_cta_newsletter', 'option');
+$link_cta_newsletter = get_field('link_cta_newsletter', 'option');
+
+$lista_contatos_ouvidoria = get_field('lista_contatos_ouvidoria', 'option');
 
 ?>
 
@@ -37,13 +39,13 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="text">
-                            <h2><?php echo $titulo_newsletter;?></h2>
-                            <p><?php echo $descricao_newsletter;?></p>
-                            <a href="<?php echo $link_cta_newsletter;?>" class="btn-padrao">
-                                <?php echo $texto_cta_newsletter;?>
+                            <h2><?php echo $titulo_newsletter; ?></h2>
+                            <p><?php echo $descricao_newsletter; ?></p>
+                            <a href="<?php echo $link_cta_newsletter; ?>" class="btn-padrao">
+                                <?php echo $texto_cta_newsletter; ?>
                             </a>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
@@ -56,15 +58,15 @@
                         <div class="menu-footer">
                             <h3>Menu</h3>
                             <?php
-                                wp_nav_menu([
-                                    'menu'            => 'Menu Fotter',
-                                    'theme_location'  => 'menu_footer',
-                                    'container'       => 'div',
-                                    'container_id'    => '',
-                                    'container_class' => '',
-                                    'menu_id'         => false,
-                                    'menu_class'      => 'navbar-nav mr-auto',
-                                ]);
+                            wp_nav_menu([
+                                'menu' => 'Menu Fotter',
+                                'theme_location' => 'menu_footer',
+                                'container' => 'div',
+                                'container_id' => '',
+                                'container_class' => '',
+                                'menu_id' => false,
+                                'menu_class' => 'navbar-nav mr-auto',
+                            ]);
                             ?>
 
                         </div>
@@ -73,34 +75,43 @@
                     <div class="col-lg-3">
                         <div class="endereco-footer">
                             <h3>Endereço</h3>
-                            <a href="<?php echo $link_endereco;?>" target="_blank"><?php echo $endereco;?></a>
+                            <a href="<?php echo $link_endereco; ?>" target="_blank"><?php echo $endereco; ?></a>
                         </div>
                     </div>
 
                     <div class="col-lg-4">
                         <div class="contato-footer">
                             <h3>Contatos</h3>
-                            <?php foreach($lista_contatos as $item): ?>
-                                    <a href="<?php echo $item['link'];?>" class="contato-item" target="_blank">
-                                        <?php echo $item['svg']; ?>
-                                        <p><?php echo $item['contato']; ?></p>
-                                    </a>
+                            <?php foreach ($lista_contatos as $item): ?>
+                                <a href="<?php echo $item['link']; ?>" class="contato-item" target="_blank">
+                                    <?php echo $item['svg']; ?>
+                                    <p><?php echo $item['contato']; ?></p>
+                                </a>
                             <?php endforeach; ?>
+
+                            <h3>Ouvidoria</h3>
+                            <?php foreach ($lista_contatos_ouvidoria as $item): ?>
+                                <a href="<?php echo $item['link']; ?>" class="contato-item" target="_blank">
+                                    <?php echo $item['svg']; ?>
+                                    <p><?php echo $item['contato']; ?></p>
+                                </a>
+                            <?php endforeach; ?>
+
                         </div>
                     </div>
 
                     <div class="col-lg-3">
                         <h3>LGPD</h3>
-                       <?php
-                            wp_nav_menu([
-                                'menu'            => 'Menu LGPD',
-                                'theme_location'  => 'menu_lgpd',
-                                'container'       => 'div',
-                                'container_id'    => '',
-                                'container_class' => '',
-                                'menu_id'         => false,
-                                'menu_class'      => 'navbar-nav mr-auto',
-                            ]);
+                        <?php
+                        wp_nav_menu([
+                            'menu' => 'Menu LGPD',
+                            'theme_location' => 'menu_lgpd',
+                            'container' => 'div',
+                            'container_id' => '',
+                            'container_class' => '',
+                            'menu_id' => false,
+                            'menu_class' => 'navbar-nav mr-auto',
+                        ]);
                         ?>
                     </div>
                 </div>
@@ -117,7 +128,8 @@
                             <div class="content-final">
                                 <div class="logo-footer">
                                     <a href="<?php echo get_home_url(); ?>">
-                                        <img src="<?php echo $logo_png_branco['url']?>" alt="<?php echo $logo_png_branco['alt']?>">
+                                        <img src="<?php echo $logo_png_branco['url'] ?>"
+                                            alt="<?php echo $logo_png_branco['alt'] ?>">
                                     </a>
                                 </div>
 
@@ -126,15 +138,15 @@
                                 <div class="redes-sociais">
                                     <p>Siga-nos:</p>
                                     <ul>
-                                        <?php foreach($redes as $item){
+                                        <?php foreach ($redes as $item) {
                                             ?>
-                                                <li>
-                                                    <a href="<?php echo $item['link_icone']?>" target="_blank">
-                                                        <?php echo $item['svg']?>
-                                                    </a>
-                                                </li>
+                                            <li>
+                                                <a href="<?php echo $item['link_icone'] ?>" target="_blank">
+                                                    <?php echo $item['svg'] ?>
+                                                </a>
+                                            </li>
                                             <?php
-                                        }?>
+                                        } ?>
                                     </ul>
                                 </div>
                             </div>
@@ -142,8 +154,8 @@
                             <div class="assinatura">
                                 <h2>
                                     <a href="http://www.atratis.com.br" target="_blank" class="ir"
-                                    title="Site criado pela agência Atratis Digital de Fortaleza - Ceará. Inbound Marketing, Criação de Sites, Mídias Sociais e mais.">Site
-                                    criado por Atratis, uma agência de comunicação digital de Fortaleza - Ceará</a>
+                                        title="Site criado pela agência Atratis Digital de Fortaleza - Ceará. Inbound Marketing, Criação de Sites, Mídias Sociais e mais.">Site
+                                        criado por Atratis, uma agência de comunicação digital de Fortaleza - Ceará</a>
                                 </h2>
                             </div>
                         </div>
@@ -154,9 +166,9 @@
             </div>
         </div>
 
-        
+
     </div>
-  
+
 </footer>
 
 
@@ -174,13 +186,14 @@
 
 <script src="https://cdn.plyr.io/3.7.8/plyr.js"></script>
 <script>
-  document.addEventListener("DOMContentLoaded", () => {
-    const players = Array.from(document.querySelectorAll('.player')).map(p => new Plyr(p));
-  });
+    document.addEventListener("DOMContentLoaded", () => {
+        const players = Array.from(document.querySelectorAll('.player')).map(p => new Plyr(p));
+    });
 </script>
 
 <?php echo get_field('acf-rodape-codigo', 'option'); ?>
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
